530c64bdcf357cec7990f42c0b4887f92aab863e
cthomas.mail <cthomas.mail@be266342-eca9-11dd-a550-c997509ed985>
2009-02-10 18:57:46 +0000

    This is Clint's working revision 2/10/2009 1:56pm
    Preliminary Sensor2Data
    Added class for Sensor3Data
    Modified sensors for standards compliance

--- trunk/engineTest/engineTest/Sensor1Data.cpp
+++ trunk/engineTest/engineTest/Sensor1Data.cpp
@@ -1,10 +1,17 @@
 #include "Sensors.h"
 
 
-Sensor1Data::Sensor1Data(int nf,double a){
-angle = a;
-numFeelers = nf;
-feelerDistances = new float[nf];
+Sensor1Data::Sensor1Data(){
+	//default
 }
 
+Sensor1Data::Sensor1Data(int nf,double a){
+	angle = a;
+	numFeelers = nf;
+	feelerDistances = new float[nf];
+}
+
+Sensor1Data::~Sensor1Data(){
+	//destructor
+}
 
---
+++ trunk/engineTest/engineTest/Sensor2Data.cpp
@@ -0,0 +1,18 @@
+#include "Sensors.h"
+
+
+Sensor2Data::Sensor2Data(){
+	//default
+}
+
+Sensor2Data::Sensor2Data(int id, double rD, double rH){
+	//Assign values to Sensor2Data object
+	agentID = id;
+	relDistance = rD;
+	relHeading = rH;
+}
+
+
+Sensor2Data::~Sensor2Data(){
+	//destructor
+}
