09c46ce9b041983f20275d3301a302597599df50
cthomas.mail <cthomas.mail@be266342-eca9-11dd-a550-c997509ed985>
2009-02-13 08:00:25 +0000

    Removed std::cout debug code

--- trunk/engineTest/engineTest/main.cpp
+++ trunk/engineTest/engineTest/main.cpp
@@ -31,7 +31,7 @@
 
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
-//#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
 
